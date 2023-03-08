06f27eeaf8059fd7e3dd29e475da134cae9856a6
Gostev, Philipp <philipp.gostev@adesso.de>
2023-03-08 13:33:35 +0100

    Refactoring Pets into another package

--- src/main/java/com/springframework/pets/CatPetService.java
+++ src/main/java/com/springframework/pets/CatPetService.java
@@ -1,13 +1,13 @@
-package guru.springframework.sfgdi.services;
+package com.springframework.pets;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile({"dog", "default"})
-public class DogPetService implements PetService{
+@Profile("cat")
+public class CatPetService implements PetService{
     @Override
     public String getPetType() {
-        return "Dogs Are the Best!";
+        return "Cats Are the Best!";
     }
 }
